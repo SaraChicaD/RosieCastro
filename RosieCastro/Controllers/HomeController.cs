9aa39193fafaacf4628dc0e227102455c65b0882
SaraChicaD <saraines.calderon@gmail.com>
2015-06-06 23:11:25 -0500

    Updates to UI. Removed social media buttons on the bottom; added photo.

--- RosieCastro/Controllers/HomeController.cs
+++ RosieCastro/Controllers/HomeController.cs
@@ -8,28 +8,28 @@ namespace RosieCastro.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your home page.";
 
             return View();
         }
 
         public ActionResult Home()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your homepage.";
 
             return View();
         }
